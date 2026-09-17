d47d9ad099937cb110c4d9c12cbe35df4fe000d3
Thioye <thioyehassimiou@gmail.com>
2026-09-17 17:06:55 +0000

    fix(release): isolate Compress-Archive into dedicated step using wildcard pattern

--- distribution/assets/js/config.js
+++ distribution/assets/js/config.js
@@ -13,8 +13,8 @@ const CONFIG = {
   LICENSE_TRIAL: "7 jours d'essai gratuit complet (100% hors-ligne)",
   
   // Liens de téléchargement du logiciel Windows & Multiplateforme (Installeur ZIP Propre - Contient uniquement NMaShop_Setup_v1.0.0.exe)
-  DOWNLOAD_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.6/NMaShop_Installer.zip",
-  DOWNLOAD_PORTABLE_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.6/NMaShop_Windows_Portable_v1.0.0.zip",
+  DOWNLOAD_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.7/NMaShop_Installer.zip",
+  DOWNLOAD_PORTABLE_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.7/NMaShop_Windows_Portable_v1.0.0.zip",
   DOWNLOAD_LINUX_URL: "downloads/nmashop_linux_release.tar.gz",
   DOWNLOAD_APK_URL: "https://drive.google.com/uc?export=download&id=1yHueBEciq3z7Xh4dKOafJoY3OHN3ZsAN",
   
