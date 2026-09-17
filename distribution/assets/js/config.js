1245755a661ad8f3f19f5a197c43971fd27722de
Thioye <thioyehassimiou@gmail.com>
2026-09-17 15:19:46 +0000

    chore(distribution): default DOWNLOAD_URL to .zip archive to bypass browser blocks

--- distribution/assets/js/config.js
+++ distribution/assets/js/config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   LAST_UPDATE: "Septembre 2026",
   LICENSE_TRIAL: "7 jours d'essai gratuit complet (100% hors-ligne)",
   
-  // Liens de téléchargement du logiciel Windows & Multiplateforme (Release Directe GitHub)
-  DOWNLOAD_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.3/NMaShop_Setup_v1.0.0.exe",
+  // Liens de téléchargement du logiciel Windows & Multiplateforme (ZIP Facile Sans Blocage)
+  DOWNLOAD_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.3/NMaShop_Windows_Portable_v1.0.0.zip",
   DOWNLOAD_PORTABLE_URL: "https://github.com/thioyehassimiou-source/N-MA-SHOP/releases/download/v1.0.3/NMaShop_Windows_Portable_v1.0.0.zip",
   DOWNLOAD_LINUX_URL: "downloads/nmashop_linux_release.tar.gz",
   DOWNLOAD_APK_URL: "https://drive.google.com/uc?export=download&id=1yHueBEciq3z7Xh4dKOafJoY3OHN3ZsAN",
